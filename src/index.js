7b7632bebbe8c826bdc5269bd32c529b88f37547
Ihor Pylypyak <ihorpylypyak@gmail.com>
2021-09-02 22:24:51 +0300

    FEAT: leave function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import './styles/main.scss'
 import { io } from 'socket.io-client'
 
 const isDev = process.env.NODE_ENV === 'development'
-
 const URL = isDev ? 'http://localhost:3033' : ''
 const socket = io(URL, { autoConnect: false }).connect()
+
+const $leave = document.querySelector('.chat__leave')
+
+$leave?.addEventListener('click', () => window.location.replace('index.html'))
